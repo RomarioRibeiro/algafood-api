53e7885a0a0ba6e0064bdf2650ffc975ab0f1dea
Romario Ribeiro <romaryjane@hotmail.com>
2024-11-18 14:46:23 -0300

    Refatorando  codigo  usando  ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -7,18 +8,16 @@ import com.algaworks.algafood.di.modelo.Cliente;
 
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador{
-	
-	@Value("${notificador.email.host-servidor}")
-	private  String host;
-	@Value("${notificador.email.porta-servidor}")
-	private Integer port;
-	
+public class NotificadorEmail implements Notificador {
+
+	@Autowired
+	private NotificadorProperties properties;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + host);
-		System.out.println("Port: " + port);
-		
-		System.out.printf("Notificando %s através do email %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem) ; 
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Port: " + properties.getPortServidor());
+
+		System.out.printf("Notificando %s através do email %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 }
