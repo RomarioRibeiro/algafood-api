3cb47a90f4932a8d904c8d50c471963677fef5a6
Romario Ribeiro <romaryjane@hotmail.com>
2024-11-27 09:41:47 -0300

    adicionando lombok nas class e criando

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+public class RestauranteRepositoryImpl implements RestauranteRepository{
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Restaurante> todos() {
+		return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
+	}
+
+	@Override
+	public Restaurante porId(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+
+	@Override
+	@Transactional
+	public Restaurante adicionar(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+
+	@Override
+	@Transactional
+	public void remover(Restaurante restaurante) {
+		restaurante = porId(restaurante.getId());
+		manager.remove(restaurante);
+	}
+
+}
