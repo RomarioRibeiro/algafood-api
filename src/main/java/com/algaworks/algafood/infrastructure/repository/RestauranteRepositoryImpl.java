bcb4de840eed357c4298e5f89147cdbd104683b4
Romario Ribeiro <romaryjane@hotmail.com>
2024-11-27 16:05:00 -0300

    criando  relacionamento das class e configurando innoDB

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -6,9 +6,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Repository;
+
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
-
+@Repository
 public class RestauranteRepositoryImpl implements RestauranteRepository{
 	
 	@PersistenceContext
