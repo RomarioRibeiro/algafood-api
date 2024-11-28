9c9c84975a563f965ef641f079e7595f1a93814d
Romario Ribeiro <romaryjane@hotmail.com>
2024-11-28 17:51:49 -0300

    Adicionando outras class  e  fazendo relacionamento delas

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/PermissaoRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.repository.PermissaoRepository;
+
+@Repository
+public class PermissaoRepositoryImpl implements PermissaoRepository{
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Permissao> todos() {
+		return manager.createQuery("from Permissao", Permissao.class).getResultList();
+	}
+	@Override
+	public Permissao porId(Long id) {
+		return manager.find(Permissao.class, id);
+	}
+	
+	@Transactional
+	@Override
+	public Permissao adicionar(Permissao permissao) {
+		return manager.merge(permissao);
+	}
+	
+	@Transactional
+	@Override
+	public void remover(Permissao permissao) {
+		permissao = porId(permissao.getId());
+		manager.remove(permissao);
+	}
+	
+}
