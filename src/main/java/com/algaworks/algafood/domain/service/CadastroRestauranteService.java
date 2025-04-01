1df36bba7cb6f0870b4f19e2c2aecde3433809fa
Romario Ribeiro <romaryjane@hotmail.com>
2025-03-31 22:05:22 -0300

    Implementando JPA com metodos do CRUD

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -20,11 +20,8 @@ public class CadastroRestauranteService {
 	
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
-		Cozinha cozinha = cozinhaRepository.buscarId(cozinhaId);
-		
-		if(cozinha == null) {
-			throw new EntidadeNaoEncontradaException(String.format("Não encontrada cozinha com código %d", cozinhaId));
-		}
+		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException(String.format("Não encontrada cozinha com código %d", cozinhaId)));
 		
 		restaurante.setCozinha(cozinha);
 		return  restauranteRepository.adicionar(restaurante);
