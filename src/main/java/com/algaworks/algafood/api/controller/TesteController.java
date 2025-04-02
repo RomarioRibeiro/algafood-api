4958793a61faddad357ee24fefc1a2c1fb36fa59
Romario Ribeiro <romaryjane@hotmail.com>
2025-04-01 21:28:55 -0300

    Criando uma consulta passando o Nome

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
@@ -13,10 +19,15 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
-//	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findBynome(nome);
+	}
+	
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findBuscarPorNomeBynome(nome);
+	}
+	
 	
 }
